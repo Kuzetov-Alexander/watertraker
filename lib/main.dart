a0452c5a43ad2237be39bbe9207c1434f96a0436
Kuzetov-Alexandr <107497862+Kuzetov-Alexandr@users.noreply.github.com>
2023-01-06 21:44:03 +0500

    06.01.2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:watertraker/features/account/presentation/pages/account_page.dart';
 import 'package:watertraker/features/onboarding/presentation/pages/splash_page.dart';
 
@@ -7,18 +8,38 @@ void main() {
   runApp(const MyApp());
 }
 
-// Это
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '',
-      routes: {
-        '/': (context) => const SplashPage(),
-        '/second': (context) => AccountPage(onPressed: () {}),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ToggleProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        initialRoute: '',
+        routes: {
+          '/': (context) => const SplashPage(),
+          '/second': (context) => AccountPage(onPressed: () {}),
+        },
+      ),
     );
+  }
+}
+
+class ToggleProvider extends ChangeNotifier {
+  double weightText = 4;
+  double heightText = 40;
+  double age = 1;
+  // double valueProvider = 0;
+
+  void recalculation() {
+    weightText = weightText * 2.2;
+    heightText = heightText * 0.033;
+
+    notifyListeners();
   }
 }
